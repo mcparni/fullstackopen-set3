efee195750f2a5ef3f1c16d9f10d8e00559b0337
Miika Pärni <miika.parni@helsinki.fi>
2018-06-14 15:18:49 +0300

    added frontend

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const logFormat = ':method :url :resp :status :res[content-length] - :response-t
 app.use(morgan(logFormat))
 
 app.use(bodyParser.json())
+app.use(express.static('build'))
 
 let persons = [
   {
